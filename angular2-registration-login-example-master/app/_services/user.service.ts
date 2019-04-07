148e4d2f839824a0724bc4fa1004d841722dbf09
amolpatil7863 <amolpatil7863@gmail.com>
2019-04-07 23:38:45 +0530

    Initial commit

---
+++ angular2-registration-login-example-master/app/_services/user.service.ts
@@ -0,0 +1,32 @@
+﻿import { Injectable, ɵConsole } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../_models/index';
+
+@Injectable()
+export class UserService {
+    
+
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>('/api/users');
+    }
+
+    getById(id: number) {
+        return this.http.get('/api/users/' + id);
+    }
+
+    create(user: User) {
+        console.log("saving user"+JSON.stringify(user));
+        return this.http.post('http://localhost:8084/api/users/signup', user);
+    }
+
+    update(user: User) {
+        return this.http.put('/api/users/' + user.id, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id);
+    }
+}
